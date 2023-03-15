f41b03aaf57e2da8d459b8ff810229929228dfc2
Andrés Rozo <80934853+rozoandrescamilo@users.noreply.github.com>
2023-03-15 05:41:16 -0500

    Update POstatusValidator.sol

--- POstatusValidator.sol
+++ POstatusValidator.sol
@@ -33,7 +33,9 @@ contract StatusValidator {
     mapping(uint256 => Step[]) public POvalidator;
 
     // Una función para registrar una PO que contendrá steps.
-    function registerPO(uint256 POID) public returns (bool success) {
+    function registerPO(address userWallet, uint256 POID) public returns (bool success) {
+        // Comprueba si la dirección de la wallet del usuario es igual a la del usuario que intenta interactuar
+        require(userWallet == msg.sender, "To be able to interact with your Purchase Order you must use your registered wallet address");
         // Comprueba que la PO no haya sido registrado previamente.
         require(POvalidator[POID].length == 0, "This product already exists");
         // Agrega un paso inicial al producto con el estado "TO_BE_CONFIRMED".
@@ -43,10 +45,12 @@ contract StatusValidator {
     }
 
     // Una función para registrar un nuevo paso en el flujo del proceso.
-    function registerStep(uint256 POID, string calldata metadata, uint256 poType) public returns (bool success) {
-         // Comprueba que la PO haya sido registrado previamente.
+    function registerStep(address userWallet, uint256 POID, string calldata metadata, uint256 poType) public returns (bool success) {
+        // Comprueba si la dirección de la wallet del usuario es igual a la del usuario que intenta interactuar
+        require(userWallet == msg.sender, "To be able to interact with your Purchase Order you must use your registered wallet address");
+        // Comprueba que la PO haya sido registrado previamente.
         require(POvalidator[POID].length > 0, "This Purchase Order doesn't exist");
-         // Comprueba que los tipos orden sean los correctos.
+        // Comprueba que los tipos orden sean los correctos.
         require(poType == 1 || poType == 2 || poType == 3, "Invalid PO type");
         // Obtiene la matriz de pasos actual para el producto.
         Step[] memory stepsArray = POvalidator[POID];
@@ -58,15 +62,15 @@ contract StatusValidator {
         }
         // Dado el tipo de estado 1 se da el máximo estado posible 2
         if (poType == 1 && currentStatus > 2) {
-            revert("The maximum status for poType 1 is 2 BOOKING REQUEST");
+            revert("The maximum status for poType 1 is 2");
         }
         // Dado el tipo de estado 2 se da el máximo estado posible 4
         if (poType == 2 && currentStatus > 5) {
-            revert("The maximum status for poType 2 is 4 BOOKING REQUEST");
+            revert("The maximum status for poType 2 is 4");
         }
         // Dado el tipo de estado 3 se da el máximo estado posible 6
         if (poType == 3 && currentStatus > 7) {
-            revert("The maximum status for poType 3 is 6 BOOKING REQUEST");
+            revert("The maximum status for poType 3 is 6");
         }
         // Se asigna el estado actual + 1 y se añade al mapping de PO
         Step memory step = Step(Status(currentStatus), metadata);
